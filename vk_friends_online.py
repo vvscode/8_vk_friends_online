31a93a16dbc9ede2ce9ec6118f7b7093f1a9a001
Vasiliy Vanchuk (@vvscode) <v.vanchuk@tut.by>
2019-01-24 15:31:06 +0400

    Fix notices

--- vk_friends_online.py
+++ vk_friends_online.py
@@ -3,8 +3,9 @@ import os
 import sys
 import getpass
 
-APP_ID = os.environ.get('VK_APP_ID')
+APP_ID = os.environ.get("VK_APP_ID")
 API_VERSION = 5.73
+
 
 def get_user_login():
     return input("Login: ")
@@ -19,15 +20,10 @@ def get_online_friends(login, password):
         app_id=APP_ID,
         user_login=login,
         user_password=password,
-        scope='friends'
+        scope="friends",
     )
-    api = vk.API(session)
-    online_friends_ids = list(api.friends.getOnline(v=API_VERSION))
-    if not online_friends_ids:
-        return []
-
-    user_ids_string = ','.join(map(str, online_friends_ids))
-    online_friends = api.users.get(user_ids=user_ids_string,v=API_VERSION)
+    api = vk.API(session, v=API_VERSION)
+    online_friends = api.users.get(user_ids=api.friends.getOnline())
     return online_friends
 
 
@@ -36,7 +32,8 @@ def output_friends_to_console(friends_online):
     for friend in friends_online:
         print("# {id} - {first_name} {last_name}".format(**friend))
 
-if __name__ == '__main__':
+
+if __name__ == "__main__":
     login = get_user_login()
     password = get_user_password()
 
